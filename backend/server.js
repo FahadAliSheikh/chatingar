6ca56b66c06728a249f5984c39fbeea6ff0afc54
FahadAliSheikh <fahad.a.sheikh@gmail.com>
2023-04-16 08:33:20 +0500

    backend code added

---
+++ backend/server.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+
+messageRoutes;
+
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const app = express();
+dotenv.config();
+connectDB();
+
+app.use(express.json());
+// Enable CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("API is running!");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
+  socket.on("setUp", (userData) => {
+    socket.join(userData._id);
+    console.log(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("user joined room:", room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageReceived) => {
+    let chat = newMessageReceived.chat;
+    if (!chat.users) return console.log("chat.users not defined!");
+    chat.users.forEach((user) => {
+      if (user._id == newMessageReceived.sender._id) return;
+      socket.in(user._id).emit("message received", newMessageReceived);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("User disconnected!");
+    socket.leave(userData._id);
+  });
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,24 +15,21 @@ import { ChatLayoutPage } from "@pages/chat";
 // const socket = io("http://localhost:4000");
 
 function App() {
-  // const socket = useRef(io("ws://localhost:8900"));
-  useEffect(() => {
-    // socket.current.emit("addOnlineUsers", "user to be added");
-  }, []);
-
   return (
     // <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-white">
     // <div className="max-w-md w-full space-y-8">
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="/home" element={<HomePage />} />
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
 
-        <Route path="/signin" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/chat" element={<ChatLayoutPage />} />
-      </Route>
-    </Routes>
+          <Route path="/signin" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/chat" element={<ChatLayoutPage />} />
+        </Route>
+      </Routes>
+    </>
     // </div>
     // </div>
   );
