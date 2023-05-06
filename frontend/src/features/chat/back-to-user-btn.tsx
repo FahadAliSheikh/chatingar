cb7ef4c0e1645696e28fc831af424a54fa80ffa9
FahadAliSheikh <fahad.a.sheikh@gmail.com>
2023-05-06 17:19:18 +0500

    back to users button design and responsiveness done

--- frontend/src/features/chat/back-to-user-btn.tsx
+++ frontend/src/features/chat/back-to-user-btn.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { setDisplayedClasses } from "@/store/slices/displaySlice";
 import { useDispatch } from "react-redux";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { FaSearch, FaInbox, FaArrowLeft } from "react-icons/fa";
 
 export function BackToUserBtn() {
   const dispatch = useDispatch();
@@ -14,8 +16,12 @@ export function BackToUserBtn() {
     );
   };
   return (
-    <button className="bg-purple-500" onClick={handleClick}>
-      Back
+    <button
+      className="bg-purple-500 flex flex-row gap-2 rounded-md hover:text-gray-400 my-4 p-2 text-white md:hidden lg:hidden"
+      onClick={handleClick}
+    >
+      <FaArrowLeft size={20} />
+      Users
     </button>
   );
 }
